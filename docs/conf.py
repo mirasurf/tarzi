77c7a6a11178e2efbbb200ea8a49d7f276622322
xmingc <chenxm35@gmail.com>
2025-07-01 23:00:59 +0800

    update [skip ci]

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 # The suffix(es) of source filenames.
 source_suffix = {
